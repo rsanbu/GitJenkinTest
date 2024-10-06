7e43123907529b3d4ad678d358bc10a998a224c0
rsanbu <rsanbu.ansi@gmail.com>
2024-10-06 17:19:12 +0530

    First Commit

---
+++ src/test/java/bingTest/SampleBingTest.java
@@ -0,0 +1,33 @@
+package bingTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleBingTest {
+
+	@Test
+	public void validateBing() throws IOException {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver wd=new ChromeDriver();
+		wd.get("https://www.bing.com/");
+		WebElement input=wd.findElement(By.name("q"));
+		input.sendKeys("chennai");
+		input.submit();
+		File src=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(src, new File("C:/Users/Lenovo P43s/eclipse-workspaceAnbu/F1_GitHubTest/screenshot/bing.png"));
+		wd.quit();
+
+	}
+	}
